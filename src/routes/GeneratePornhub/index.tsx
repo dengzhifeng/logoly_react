e46e787786cab329ee194b0504b4a9c833997175
邓志锋 <398577351@qq.com>
2021-12-15 07:52:32 +0800

    update

--- src/routes/GeneratePornhub/index.tsx
+++ src/routes/GeneratePornhub/index.tsx
@@ -3,17 +3,25 @@
  * @author: steve.deng
  * @Date: 2021-12-14 16:12:41
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-12-14 18:08:39
+ * @LastEditTime: 2021-12-15 07:52:32
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
 
 function GeneratePornhub() {
+    function download() {
+        console.log('download');
+        const node = document.getElementById('logo')
+        //   domtoimage.toPng(node).then(function(res) {
+        //     console.log(res)
+        //     that.downloadImage(res, 'logo.png')
+        //   })
+    }
     return (
         <div className="pornhub container">
             <div className="box">
-                <div className="editarea">
+                <div className="editarea" id="logo">
                     <span
                         className="prefix"
                         contentEditable={true}
@@ -29,6 +37,12 @@ function GeneratePornhub() {
                         xx
                     </span>
                 </div>
+            </div>
+            <div className="download-share">
+                <div
+                    className="download"
+                    onClick={download}
+                >Export</div>
             </div>
         </div>
     );
