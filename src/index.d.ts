eceea3a6d91b25d3987a43a271401ba346208433
dengzhifeng <398577351@qq.com>
2021-12-16 18:18:41 +0800

    update

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @description:
+ * @author: steve.deng
+ * @Date: 2021-12-16 18:13:19
+ * @LastEditors: steve.deng
+ * @LastEditTime: 2021-12-16 18:13:21
+ */
+declare module 'dom-to-image';
--- src/routes/GeneratePornhub/index.tsx
+++ src/routes/GeneratePornhub/index.tsx
@@ -3,21 +3,43 @@
  * @author: steve.deng
  * @Date: 2021-12-14 16:12:41
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-12-15 07:52:32
+ * @LastEditTime: 2021-12-16 18:15:59
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import domToImage from 'dom-to-image';
 import './index.less';
-
 function GeneratePornhub() {
     function download() {
         console.log('download');
-        const node = document.getElementById('logo')
-        //   domtoimage.toPng(node).then(function(res) {
-        //     console.log(res)
-        //     that.downloadImage(res, 'logo.png')
-        //   })
+        const node = document.getElementById('logo');
+        domToImage.toPng(node).then(function (res: string) {
+            console.log(res);
+            downloadImage(res, 'logo.png');
+        });
     }
+
+    function downloadImage(imgsrc: string, name: string) {
+        //下载图片地址和图片名
+        let image = new Image();
+        // 解决跨域 Canvas 污染问题
+        image.setAttribute('crossOrigin', 'anonymous');
+        image.onload = function () {
+            let canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+            let context = canvas.getContext('2d');
+            context.drawImage(image, 0, 0, image.width, image.height);
+            let url = canvas.toDataURL('image/png');
+            let a = document.createElement('a');
+            let event = new MouseEvent('click');
+            a.download = name || 'photo';
+            a.href = url;
+            a.dispatchEvent(event);
+        };
+        image.src = imgsrc;
+    }
+
     return (
         <div className="pornhub container">
             <div className="box">
@@ -39,10 +61,9 @@ function GeneratePornhub() {
                 </div>
             </div>
             <div className="download-share">
-                <div
-                    className="download"
-                    onClick={download}
-                >Export</div>
+                <div className="download" onClick={download}>
+                    Export
+                </div>
             </div>
         </div>
     );
